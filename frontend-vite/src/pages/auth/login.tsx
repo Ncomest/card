e8666da0b59b92ec9a7d693e73633a35e4408701
Hurtik Igor <hurtik.igor@gmail.com>
2026-08-01 00:04:57 +0300

    Changes(Button): вернул старую кнопку. Uninstall(MUI) Upd (package.json)

--- frontend-vite/src/pages/auth/login.tsx
+++ frontend-vite/src/pages/auth/login.tsx
@@ -224,7 +224,7 @@ const Login = () => {
               />
             </InputContainerStyle>
           </LabelStyle>
-          <Button type="system" size="xxl" onClick={(e) => handleSubmit(e)}>
+          <Button type="system" size="xxl">
             Войти
           </Button>
         </FormStyle>
